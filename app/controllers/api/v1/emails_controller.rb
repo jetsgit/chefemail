c8d6878fcda49c80623c86e4b302930cf08dcbed
Jerrold Thompson <jet@whidbey.com>
2016-05-03 09:09:30 -0700

    configure scoped namespace for email api

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -3,7 +3,7 @@ module Api
     class EmailsController < ApplicationController
       respond_to :json
       def index
-        respond_with Email.where("id > ?", 500).pluck(:email, :id).make_hash
+        respond_with Email.where("id > ?", 500).pluck(:email, :id).make_hash, content_type: 'application/chefemail; v=1'
       end
     end
   end
