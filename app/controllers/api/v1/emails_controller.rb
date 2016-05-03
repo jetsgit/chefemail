5693befd936c5b491b94a8094506d4fa6f75ffd4
Jerrold Thompson <jet@whidbey.com>
2016-05-02 23:44:02 -0700

    update email api controller

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -3,7 +3,7 @@ module Api
     class EmailsController < ApplicationController
       respond_to :json
       def index
-        @emails = Email.where("id > ?", 500).pluck(:email, :id).make_hash
+        respond_with Email.where("id > ?", 500).pluck(:email, :id).make_hash
       end
     end
   end
