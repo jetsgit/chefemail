90c390c5a52b5a86d40e0a2e36b60a0b28f77884
Jerrold Thompson <jet@whidbey.com>
2016-05-02 18:15:24 -0700

    Array#make_hash, needs to be moved!

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,4 +2,15 @@ class Email < ActiveRecord::Base
   def self.pluck_to_hash(keys)
     pluck(*keys).map{|pa| Hash[keys.zip(pa)]}
   end
+
+  # create json hash out of array of :email/:id arrays
+end
+
+class Array < Object
+  def make_hash
+    self.inject({}) do |result, el|
+      result[el.first] = el.last
+      result
+    end
+  end
 end
