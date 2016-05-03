285c83f17f4eb260bf36c9ef74eb9ff3230f707d
Jerrold Thompson <jet@whidbey.com>
2016-05-02 23:24:00 -0700

    create namespace for api in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do 
+      resources :emails, only: [:index]
+    end
+  end
   resources :emails
   root 'emails#index'
 end
