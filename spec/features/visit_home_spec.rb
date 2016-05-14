4f750fdd2a74582f667eb932d3a4f9662a06474b
Jerrold Thompson <jet@whidbey.com>
2016-05-14 13:42:28 -0700

    finish visit_home feature spec

--- spec/features/visit_home_spec.rb
+++ spec/features/visit_home_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper.rb'
 
 feature "Visit home page", js: true do
+  before {create :email, email: "john.jaspers@example.com"}
   scenario "User goes to app root" do
     visit '/'
     expect(page).to have_content 'Emails'
+    expect(page).to have_content "john.jaspers@example.com" 
+    expect(page).to have_no_content "joni.mitchell@alberta.com"
   end
 end
