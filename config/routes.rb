eadf8d7ba62f208cf2448640ec74b66d0a70ed01
Jerrold Thompson <jet@whidbey.com>
2016-06-25 15:43:52 -0700

    changing routes to jewels from bad name of gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ require 'api_constraints'
 Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
-      resources :emails, only: [:index]
+      resources :jewels, only: [:index]
     end
   end
-  resources :emails
+  resources :jewels
   root 'emails#index'
 end
