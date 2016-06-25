1185c0dd70e18d337bbbb2de71c406c258261e72
Jerrold Thompson <jet@whidbey.com>
2016-06-25 15:45:32 -0700

    revert to emails, my bad!

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ require 'api_constraints'
 Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
-      resources :jewels, only: [:index]
+      resources :emails, only: [:index]
     end
   end
-  resources :jewels
+  resources :emails
   root 'emails#index'
 end
